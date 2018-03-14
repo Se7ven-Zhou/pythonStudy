a6e364dfa54cf610aff06fb92efe789ec0af5a60
Se7ven-Zhou <406574570@qq.com>
2018-03-14 18:00:44 +0800

    up

--- venv/HomeWork/unittest_0314_excel/read_data_by_Excel.py
+++ venv/HomeWork/unittest_0314_excel/read_data_by_Excel.py
@@ -11,16 +11,18 @@ class Read_data:
         file = load_workbook(self.path)
         sheet = file.get_sheet_by_name("python_test")
         params = {}
-
+        test_data_list = []
         for item in range(2, sheet.max_row + 1):
             params["mobilephone"] = sheet.cell(row=item, column=2).value
             params["pwd"] = sheet.cell(row=item, column=3).value
-            print(params)
-
+        test_data_list.append(params)
+        print(test_data_list)
+        return test_data_list
         file.save("test.xlsx")
 
 
 if __name__ == "__main__":
 
-    test_data = Read_data("test.xlsx").Get_data()
-    print(test_data)
+    # test_data = Read_data("test.xlsx").Get_data()
+    # print(test_data)
+    Read_data("test.xlsx").Get_data()
