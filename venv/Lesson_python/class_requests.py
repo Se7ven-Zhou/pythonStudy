39d647c24fcf1448ddae3bb4362193dec1c4df89
Se7ven-Zhou <406574570@qq.com>
2018-03-21 17:48:53 +0800

    up

--- venv/Lesson_python/class_requests.py
+++ venv/Lesson_python/class_requests.py
@@ -52,7 +52,7 @@ class HttpRrequsts:
 if __name__ == "__main__":
 
     url = "http://119.23.241.154:8080/futureloan/mvc/api/member/login"
-    params = {"mobile":"13752852018","password":"123456"}
+    params = {"mobilephone":"13752852018","pwd":"123456"}
     tester = HttpRrequsts(url,params)
     test = tester.Get()
     print(test)
