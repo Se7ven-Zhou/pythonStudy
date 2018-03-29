721c43b55beac64ad38748aeec4f3cb185a1e239
Se7ven-Zhou <406574570@qq.com>
2018-03-29 17:36:05 +0800

    up

--- venv/Lesson_python/class_requests.py
+++ venv/Lesson_python/class_requests.py
@@ -1,7 +1,8 @@
 # coding:utf-8
 
 import requests
-
+import time
+from openpyxl import load_workbook
 # url = "http://119.23.241.154:8080/futureloan/mvc/api/member/register"
 # params = {"mobilephone":"13100000000","pwd":"123456"}
 # result = requests.post(url,params)
@@ -45,14 +46,51 @@ class HttpRrequsts:
         response = requests.get(self.url,self.params)
         return response.json()
 
-    def Post(self):
+     def Post(self):
         response = requests.post(self.url,self.params)
         return response.json()
 
 if __name__ == "__main__":
 
-    url = "http://119.23.241.154:8080/futureloan/mvc/api/member/login"
-    params = {"mobilephone":"13752852018","pwd":"123456"}
+    # url = "http://119.23.241.154:8080/futureloan/mvc/api/member/login"
+    # params = {"mobilephone":"13752852018","pwd":"123456"}
+    # tester = HttpRrequsts(url,params)
+    # test = tester.Get()
+    # print(test)
+
+    url = "http://119.23.132.26:8091/u/login"
+    params = {"params":"{'mobile':'13752852011','password':'123456','systemName':'IOS'}","signature":"82601564dc52a8e456f7c82272ba3d09","timestamp":1522305160,"admin":"!QS#$^Tghi0"}
     tester = HttpRrequsts(url,params)
-    test = tester.Get()
+    test = tester.Post()
     print(test)
+
+# class Get_data:
+#     def __init__(self,systemName="IOS",timestamp=1522305160,signature="82601564dc52a8e456f7c82272ba3d09",admin="!QS#$^Tghi0"):
+#         self.systemName = systemName
+#         self.timestamp =timestamp
+#         self.signature = signature
+#         self.admin = admin
+#
+#     def Read_excel(self,path):
+#         file = load_workbook(path)
+#         sheet = file.get_sheet_by_name("python_test")
+#         test_data_list = []
+#
+#         for item in range(2, sheet.max_row + 1):
+#             data_params = {}
+#             params = {}
+#             data_params["mobile"] = sheet.cell(row=item, column=1).value
+#             data_params["password"] = sheet.cell(row=item, column=2).value
+#             data_params["systemName"] = self.systemName
+#
+#             params["params"] = data_params
+#             params["timestamp"] = self.timestamp
+#             params["signature"] = self.signature
+#             params["admin"] = self.admin
+#             file.save(path)
+#             print(params)
+#
+# if __name__ == "__main__":
+#
+#     data_params = Get_data().Read_excel("test.xlsx")
+#     print(data_params)
