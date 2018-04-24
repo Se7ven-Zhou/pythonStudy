5748111f172ba49649ed7fd83c97f4a21530e784
Se7ven-Zhou <406574570@qq.com>
2018-04-24 17:56:41 +0800

    up

--- venv/Lesson_python/selenium_web/python_selenium.py
+++ venv/Lesson_python/selenium_web/python_selenium.py
@@ -8,6 +8,14 @@ Xpath定位语法
 <input class="item_account" autocomplete="off" type="text" name="user" id="username" placeholder="邮箱/手机号码/小米ID">
 //标签[@属性=值]
 //input[@id="username"]
+
+ancestor    祖先结点
+parent      父节点
+preceding   当前元素结点标签之前的所有结点
+preceding-sibling   当前元素结点标签之前的所有兄弟结点
+following   当前元素标签之后的所有结点
+following-sibling   当前元素标签之后的所有兄弟结点      
+
 """
 
 class Login_test():
@@ -30,4 +38,4 @@ if __name__ == "__main__":
     Login_test().Login_xiaomi("https://www.mi.com/")
 
 
-
+# //span[text()=" lr_test"]/ancestor::a/following-sibling::div//a
