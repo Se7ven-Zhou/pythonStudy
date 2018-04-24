1ddd986ca568e1f372ea788c5a40dbf32497a693
Se7ven-Zhou <406574570@qq.com>
2018-04-24 17:29:10 +0800

    up

--- venv/Lesson_python/selenium_web/python_selenium.py
+++ venv/Lesson_python/selenium_web/python_selenium.py
@@ -1,7 +1,33 @@
 # coding:utf-8
 
+import time
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+"""
+Xpath定位语法
+<input class="item_account" autocomplete="off" type="text" name="user" id="username" placeholder="邮箱/手机号码/小米ID">
+//标签[@属性=值]
+//input[@id="username"]
+"""
 
-driver
+class Login_test():
+
+    def Login_xiaomi(self,https):
+        driver = webdriver.Firefox()
+        driver.get(https)
+
+        driver.find_element_by_link_text("登录").click()
+        driver.maximize_window()
+
+        time.sleep(2)
+        driver.find_element_by_id("username").send_keys("13752852018")
+        driver.find_element_by_id("pwd").send_keys("701777xmzj")
+        driver.find_element_by_id("login-button").click()
+        driver.close()
+
+if __name__ == "__main__":
+
+    Login_test().Login_xiaomi("https://www.mi.com/")
+
+
+
