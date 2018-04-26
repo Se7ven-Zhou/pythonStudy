13b1ea3b4b0d04674719194e8386aa27b7b80185
Se7ven-Zhou <406574570@qq.com>
2018-04-26 17:53:12 +0800

    up

--- venv/Lesson_python/selenium_web/python_selenium.py
+++ venv/Lesson_python/selenium_web/python_selenium.py
@@ -3,6 +3,10 @@
 import time
 from selenium import webdriver
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 """
 Xpath定位语法
 <input class="item_account" autocomplete="off" type="text" name="user" id="username" placeholder="邮箱/手机号码/小米ID">
@@ -28,31 +32,51 @@ driver.execute_script(js)
 
 """
 
+"""
+强制等待：   time.sleep()
+隐形等待：   implicitly_wait()
+显性等待：明确等待某个条件满足了就执行下一步，设置最长时间，抛出TimeoutException
+
+"""
+
 class Login_test():
 
-    def Login(self,https):
+    # def Login(self,https):
+    #     driver = webdriver.Firefox()
+    #     driver.get(https)
+    #     driver.implicitly_wait(30)  # 最长等待30秒，智能等待
+    #
+    #     driver.find_element_by_xpath("//a[@id='js_login']").click()
+    #     driver.find_element_by_link_text("登录").click()
+    #     # driver.maximize_window()
+    #     time.sleep(2)
+    #
+    #     driver.find_element_by_xpath("//a[@class='js-btns-enter btns-enter btns-enter-qq']").click()
+    #     driver.switch_to.frame("login_frame_qq")
+    #     time.sleep(3)
+    #     driver.find_element_by_id("switcher_plogin").click()
+    #     driver.find_element_by_id("u").send_keys("13752852018")
+    #     driver.find_element_by_id("p").send_keys("701777@xmzj")
+    #     driver.find_element_by_id("login_button").click()
+    #
+    #     time.sleep(5)
+    #     # driver.close()  # 关闭浏览器
+    #     driver.quit()   # 关闭浏览器并退出ChromeDriver
+
+    def Login_baidu(self,https):
         driver = webdriver.Firefox()
         driver.get(https)
 
-        driver.find_element_by_xpath("//a[@id='js_login']").click()
-        # driver.find_element_by_link_text("登录").click()
-        # driver.maximize_window()
-        time.sleep(2)
+        driver.find_element_by_xpath("//div[@id='u1']/a[@name='tj_login']").click()
 
-        driver.find_element_by_xpath("//a[@class='js-btns-enter btns-enter btns-enter-qq']").click()
-        driver.switch_to.frame("login_frame_qq")
-
-        # driver.find_element_by_xpath("//a[@id='switcher_plogin']").click()
-        # driver.find_element_by_id("u").send_keys("13752852018")
-        # driver.find_element_by_id("p").send_keys("701777@xmzj")
-        # driver.find_element_by_id("login_button").click()
-
-        time.sleep(5)
-        driver.close()
+        # 判断特定条件出现后再执行下一步操作，确定弹出框出现再操作
+        login_pop_id = "//p[@class='tang-pass-footerBarULogin pass-link']"
+        WebDriverWait(driver,10,1).until(EC.visibility_of_element_located((By.XPATH,login_pop_id)))
+        driver.find_element_by_xpath(login_pop_id).click()
 
 if __name__ == "__main__":
 
-    Login_test().Login("https://ke.qq.com/")
+    Login_test().Login_baidu("https://baidu.com")
 
 
 # //span[text()=" lr_test"]/ancestor::a/following-sibling::div//a
