c959560547a5a5a9f0ef8abe6a33bff3afad48a0
pythonStudy <406574570@qq.com>
2018-05-07 22:17:07 +0800

    3.4

--- venv/Lesson_python/logging_class/logging_write.py
+++ venv/Lesson_python/logging_class/logging_write.py
@@ -4,19 +4,24 @@ import logging
 import os
 import time
 
-
-
+"""取文件信息"""
 current_path = os.getcwd()
 file_name = time.strftime("%Y-%m-%d")+".txt"
 file_path = os.path.join(current_path,file_name)
-
+"""日志信息"""
 logger = logging.getLogger("python_info")
-fh = logging.FileHandler(file_path)
-logger.addFilter(fh)
+fh = logging.FileHandler(file_path,encoding = "utf-8")
+logger.addHandler(fh)
+# logger.critical("崩溃信息")
 
 
 if os.path.exists(file_path):
     file = open(file_path,"a")
-
-
+    logger.addFilter(fh)
     file.close()
+else:
+    file = open(file_path,"w")
+    logger.addFilter(fh)
+    file.close()
+
+logger.critical("崩溃信息")
