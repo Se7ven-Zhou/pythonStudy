619104b7c783c9cc7d01ec7993502827554d85f4
Se7ven-Zhou <406574570@qq.com>
2018-05-15 15:58:22 +0800

    up

--- venv/HomeWork/Web_PO_unittest/TestCases/test_login_pytest1.py
+++ venv/HomeWork/Web_PO_unittest/TestCases/test_login_pytest1.py
@@ -14,29 +14,21 @@ import pytest
 1.fixture的函数名，传递给测试用例
 """
 
-# @pytest.mark.usefixture("init_env")
-# class Test_login():
-#
-#     @pytest.fixture
-#     def init_driver(self):
-#
-#         self.driver = webdriver.Firefox()
-#         self.driver.implicitly_wait(10)
-#         yield
-#         self.driver.quit()
-#
-#     @pytest.mark.smoke
-#     def test_login_success(self,init_driver):
-#         # 测试数据
-#         mobile = "13752852018"
-#         password = "701777xmzj"
-#         username = "Se7ven"
-#
-#         # 步骤
-#         Login_page(self.driver, CommonData.url).Login_function(mobile,password)
-#         nickname = Index_page(self.driver).Login_name()
-#         # 检验
-#         assert nickname == username
+@pytest.mark.usefixture("init_driver")
+class Test_login():
+
+    @pytest.mark.smoke
+    def test_login_success(self,init_driver):
+        # 测试数据
+        mobile = "13752852018"
+        password = "701777xmzj"
+        username = "Se7ven"
+
+        # 步骤
+        Login_page(init_driver, CommonData.url).Login_function(mobile,password)
+        nickname = Index_page(init_driver).Login_name()
+        # 检验
+        assert nickname == username
 
     # def test_login_fail(self):
     #     # 测试数据
--- venv/HomeWork/Web_PO_unittest/main_smoke.py
+++ venv/HomeWork/Web_PO_unittest/main_smoke.py
@@ -7,12 +7,12 @@ import os
 
 if __name__ == "__main__":
 
-    # report_path = os.getcwd() + "/TestReports" # 测试报告地址
-    #
-    # now = time.strftime("%Y-%m-%d_%H_%M_%S")  # 获取当前时间
-    # report_name = "python" + now + ".html"
-    #
-    # test_report_path = os.path.join(report_path,report_name)
+    report_path = os.getcwd() + "/TestReports" # 测试报告地址
+
+    now = time.strftime("%Y-%m-%d_%H_%M_%S")  # 获取当前时间
+    report_name = "python" + now + ".html"
+
+    test_report_path = os.path.join(report_path,report_name)
 
 
-    pytest.main(["-m","smoke"])
+    pytest.main(["-m","smoke","--html",test_report_path])
