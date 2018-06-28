d0cbce7f49307ec1cee722fca0b3e762511f5842
Se7ven-Zhou <406574570@qq.com>
2018-06-28 18:03:18 +0800

    up

--- venv/HuShi/Common/logger.py
+++ venv/HuShi/Common/logger.py
@@ -9,43 +9,50 @@ class Logging:
 
     def __init__(self):
 
-        pass
+        self.address = os.path.split(os.getcwd())[0] + "\Logs"
+        self.log_name = time.strftime("%Y-%m-%d")+".txt"
+        self.log_path = os.path.join(self.address,self.log_name)
+
+        # 生成日志文件,日期命名，没有就创建
+        if os.path.exists(self.log_path):
+            file = open(self.log_path, "a")
+            file.close()
+        else:
+            file = open(self.log_path, "w")
+            file.close()
+
 
     def Get_Error(self,message):
 
-        address = os.path.split(os.getcwd())[0] + "\Logs"
-        log_name = time.strftime("%Y-%m-%d")+".txt"
-        log_path = os.path.join(address,log_name)
-
-        # 创建日志收集器
-        logger = logging.getLogger("Interface_Auto")  # 创建日志收集器
-        fh = logging.FileHandler(log_path, encoding="utf-8")  # 创建日志输出渠道
+        logger = logging.getLogger("Interface_Auto_Error")  # 创建日志收集器
+        fh = logging.FileHandler(self.log_path, encoding="utf-8")  # 创建日志输出渠道
         logger.addHandler(fh)  # 收集器添加输出渠道
-        # 设置日志输出格式
+        # 创建输出格式
         formatter = logging.Formatter("[%(levelname)s]-[%(asctime)s]-[%(filename)s]-[%(module)s]-[%(funcName)s]--日志信息：%(message)s")
         fh.setFormatter(formatter)
-        # fh.setLevel(level)
 
-        logger.error(message)
+        logger.error(message) # 输出内容
         logger.removeFilter(fh)
 
-        # 生成日志文件,日期命名，没有就创建
-        if os.path.exists(log_path):
-            file = open(log_path, "a")
-            file.close()
-        else:
-            file = open(log_path, "w")
-            file.close()
 
-    def Get_Info(self):
+    def Get_Info(self,message):
 
-        pass
+        logger = logging.getLogger("Interface_Auto_Info")  # 创建日志收集器
+        fh = logging.FileHandler(self.log_path, encoding="utf-8")  # 创建日志输出渠道
+        logger.addHandler(fh)  # 收集器添加输出渠道
+        # 创建输出格式
+        formatter = logging.Formatter("[%(levelname)s]-[%(asctime)s]-[%(filename)s]-[%(module)s]-[%(funcName)s]--日志信息：%(message)s")
+        fh.setFormatter(formatter)
+
+        logger.info(message) # 输出内容
+        logger.removeFilter(fh)
 
 
 if __name__ == "__main__":
 
-    try:
-        a
-    except Exception as error:
-        Logging().Get_Error(error)
-        raise
+    Logging().Get_Info("bbb")
+    # try:
+    #     aaaa
+    # except Exception as error:
+    #     Logging().Get_Error(error)
+    #     raise
