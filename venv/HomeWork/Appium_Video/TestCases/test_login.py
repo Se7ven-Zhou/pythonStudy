1c9cb7143f7af65d248847c7783c05f5a356f70f
Se7ven-Zhou <406574570@qq.com>
2018-07-24 18:01:15 +0800

    up_company

--- venv/HomeWork/Appium_Video/TestCases/test_login.py
+++ venv/HomeWork/Appium_Video/TestCases/test_login.py
@@ -12,7 +12,7 @@ class Test_login(unittest.TestCase):
 
     def setUp(self):
 
-        pass
+        print("***********************Start*********************")
 
     def tearDown(self):
 
@@ -27,25 +27,32 @@ class Test_login(unittest.TestCase):
         mobile = "13752852018"
         password = "701777xmzj"
         Login_page(self.driver).login_function(mobile,password)
-        username = Base_function(self.driver).Find_element(locator=self.username_id).text
-        assert username == "Se7ven",Logging().Error("账号不对")
+        username = Base_function(self.driver).Find_element(locator= Login_page.username_id).text
+        assert username == "Se7ven",Logging().Error("账号不对,用户名为："+ username)
 
     def test_login_Nopassword(self):
-
         mobile = "13752852018"
         password = ""
         Login_page(self.driver).login_function(mobile,password)
         error_messsage = Base_function(self.driver).Find_element(locator=Login_page.login_wrong_id).text
-        assert error_messsage == "密码不能为空",Logging().Error("账号不对")
+        assert error_messsage == "密码不能为空",Logging().Error("用例失败，提示信息："+ error_messsage)
 
     def test_login_wrongAccount(self):
-
         mobile = "13752852018"
         password = "123456"
         Login_page(self.driver).login_function(mobile,password)
         error_messsage = Base_function(self.driver).Find_element(locator=Login_page.login_wrong_id).text
-        assert error_messsage == "用户名密码不匹配",Logging().Error("账号不对")
+        assert error_messsage == "用户名密码不匹配",Logging().Error("用例失败，提示信息："+ error_messsage)
 
 if __name__ == "__main__":
 
-    unittest.main()
+    # unittest.main()
+    suite = unittest.TestSuite()
+    runner = unittest.TextTestRunner
+    # loader = unittest.TestLoader()
+    # suite.addTest(loader.loadTestsFromTestCase(Test_login))
+    suite.addTest(Test_login("test_login_wrongAccount"))
+    print(suite)
+    runner.run(suite)
+
+
